7257c46349c9322fd4041e772ab938a3dbd95508
fdirige <118780727+fdirige@users.noreply.github.com>
2023-01-25 18:29:06 -0800

    Added questions, updated CSS and Javascript

---
+++ assets/js/script3.js
@@ -0,0 +1,130 @@
+class Quiz {
+    constructor(questions) {
+        this.score = 0;
+        this.questions = questions;
+        this.questionIndex = 0;
+    }
+
+    getQuestionIndex() {
+        return this.questions[this.questionIndex];
+    }
+
+    guess(answer) {
+        if (this.getQuestionIndex().isCorrectAnswer(answer)) {
+            this.score++;
+        }
+        this.questionIndex++
+    }
+
+    isEnded() {
+        return this.questionIndex === this.questions.length;
+    }
+}
+
+// CREATE A QUESTION CLASS
+
+class Question {
+    constructor(text, choices, answer) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+    }
+
+    isCorrectAnswer(choices) {
+        return this.answer === choices;
+    }
+}
+
+//DISPLAY QUESTION
+function displayQuestion() {
+    if (quiz.isEnded()) {
+        showScores();
+    } else {
+        // SHOW NEXT QUESTION
+        let questionElement =document.getElementById("question");
+        questionElement.innerHTML = quiz.getQuestionIndex().text;
+
+        // SHOW OPTIONS
+        let choices = quiz.getQuestionIndex().choices;
+        for (let i = 0; i < choices.length; i++) {
+            let choiceElement = document.getElementById("choice"
+            +i);
+            choiceElement.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+
+        showProgress();
+    }
+};
+
+// GUESS FUNCTION
+function guess(id, guess) {
+    let button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        displayQuestion();
+    }
+}
+
+// SHOW QUIZ PROGRESS
+function showProgress() {
+    let currentQuestionNumber = quiz.questionIndex + 1;
+    let progressElement = document.getElementById("progress");
+    progressElement.innerHTML = 
+    `Question ${currentQuestionNumber} of ${quiz.questions.length}
+    `;
+}
+
+// SHOW SCORE
+function showScores() {
+    let quizEndHTML = 
+        `
+            <h1>Quiz Completed</h1>
+            <h2 id="score">You Scored: ${quiz.score} of ${quiz.questions.length}</h2>
+            <div class="quiz-repeat">
+                <a href="index3.html">Take Quiz Again</a>
+            </div>
+        `;
+        let quizElement = document.getElementById("quiz");
+        quizElement.innerHTML = quizEndHTML;
+}
+
+// CREATE QUIZ QUESTIONS
+let questions = [
+    new Question(
+        "Hyper Text Markup Language Stands For?", ["JQuery", "XHTML", "CSS", "HTML"], "HTML" ),
+    new Question(  
+        "Which is JavaScript Framework?", ["React", "Laravel", "Django", "Sass"], "React" ),
+    new Question(
+        "Which is backend language", ["PHP", "HTML", "CSS", "HTML"], "PHP" ),
+    new Question(
+        "Which is best for Artificial intelligence?", ["Sass", "Python", "CSS", "all"], "Python" ),
+];
+
+let quiz = new Quiz(questions);
+
+// DISPLAY QUESTION
+displayQuestion();
+
+// ADD A COUNTDOWN
+let time = 10;
+let quizTimer = time * 60 * 60;
+quizTime = quizTimer / 60;
+
+let counting = document.getElementById("count-down");
+
+function startCountdown() {
+    let quizTimer = setInterval(function() {
+        if (quizTime <= 0) {
+            clearInterval(quizTimer);
+            showScores();
+        } else {
+            quizTime --;
+            let sec = Math.floor(quizTime % 60);
+            let min = Math.floor(quizTime / 60) % 60;
+            counting.innerHTML = `TIME: ${min} : ${sec}`;
+        }
+    }, 1000)
+}
+
+startCountdown();
