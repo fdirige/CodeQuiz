1e32a4de914b6fc5c3a86595f4f73c8d2669901a
fdirige <118780727+fdirige@users.noreply.github.com>
2023-02-18 16:25:35 -0800

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # CodeQuiz
+
+##Description
+
+This is a 10 minute timed Coding Quiz Application. If users enters an incorrect answer, time is subtracted from the clock. After the quiz has been completed, users will input their initials and their score will be stored in local storage. Users will have the ability to view their previous scores at the end of the quiz as well. 
+
+The technologies I used are HTML, Javascript, and CSS. I used two HTML and Javascript files for the Quiz Questions and the High Scores page. CSS was used for styling my application. 
+
+The challenges I faced were subtracting time from the clock when a user enters an incorrect answer and viewing the previous scores. I originally had one HTML file and one Javascript file, but decided to have separate HTML and Javascript files for the high scores functionality. Features I hope to implement in the future is enhancing the styling of the application. 
+
+https://fdirige.github.io/CodeQuiz/
+
+## Screen shot
+
+![Screen shot of Coding Quiz](./assets/Images/Coding-Quiz.JPG) 
--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,51 +1,8 @@
-const submitInitials = document.getElementById("scores")
-submitInitials.addEventListener("click", showHighscores)
-function showHighscores() {
+console.log(JSON.parse(localStorage.getItem("highscores")));
+var scoresElement = document.querySelector(".scores");
+var highscores = JSON.parse(localStorage.getItem("highscores"));
 
-    const highscoresWrapper = document.getElementById
-        ("highscores")
-
-
-    for (let i = 0; i < scores.length; i++) {
-        const scoreInfo = scores[i];
-        highscoresWrapper.innerHTML += scoreInfo.initials + " " + scoreInfo.score + "<br>"
+    for (let i = 0; i < highscores.length; i++) {
+        const scoreInfo = highscores[i];
+        scoresElement.innerHTML += scoreInfo.initials + " " + scoreInfo.quizScore + "<br>"
     }
-
-    highscoresWrapper.classList.remove("hidden")
-}
-
-renderScore();
-
-function displayMessage(type, message) {
-    msgDiv.textContent = message;
-    msgDiv.setAttribute("class", type);
-}
-
-function renderScore() {
-    var scores = localStorage.getItem("scores");
-    var initials = localStorage.getItem("initials");
-
-    if (!scores || !initials) {
-      return;
-    }
-
-    userScoresSpan.textContent = scores;
-    userInitialsSpan.textContent = initials;
-  }
-
-//   scoreBtn.addEventListener("click", function(event) {
-//     event.preventDefault();
-
-//     var scores = document.querySelector("#scores").value;
-//     var initials = document.querySelector("#initials").value;
-
-//     if (initials === "") {
-//       displayMessage("error", "Initials cannot be blank");
-//     } else {
-//       displayMessage("success", "Succesfully entered!");
-
-//       localStorage.setItem("scores", score);
-//       localStorage.setItem("initials", initials);
-//       renderScore();
-//     }
-//   });
