3f765bc5bc7c076974bfbc2ed884871f5646ec18
fdirige <118780727+fdirige@users.noreply.github.com>
2023-02-25 12:08:28 -0800

    Updated file path for Take Quiz again link

--- script3.js
+++ script3.js
@@ -98,7 +98,7 @@ function showScores() {
             <h1>Quiz Completed</h1>
             <h2 id="score">You Scored: ${quiz.score} of ${quiz.questions.length}</h2>
             <div class="quiz-repeat">
-                <a href="index3.html">Take Quiz Again</a>
+                <a href="index.html">Take Quiz Again</a>
             </div>
             <div class="quiz-repeat">
                 <a href="index2.html">High Scores</a>
